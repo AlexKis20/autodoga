2d6bc860ebf04a1f5a801ad7cfc580b94660069f
kisrondox5@gmail.com <kisrondox5@gmail.com>
2025-04-16 09:53:01 +0200

    Add project files.

---
+++ autodoga/Auto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autodoga
+{
+    internal class Auto
+    {
+        
+
+        public string Marka { get; set; }
+        public string Tipus { get; set; }
+        public int GyartasiEv { get; set; }
+        public int UzemanyagFogyasztas { get; set; }
+        public int MegtettKilometerekSzama { get; set; }
+
+        public Auto(string marka, string tipus, int gyartasiEv, int uzemanyagFogyasztas, int megtettKilometerekSzama)
+        {
+            Marka = marka;
+            Tipus = tipus;
+            GyartasiEv = gyartasiEv;
+            UzemanyagFogyasztas = uzemanyagFogyasztas;
+            MegtettKilometerekSzama = megtettKilometerekSzama;
+        }
+
+        private int Eletkor()
+        {
+            return 2025 - GyartasiEv;
+        }
+        private int Uzemanyag_felhasznalas()
+        {
+            return MegtettKilometerekSzama / 100;
+        }
+        private int Atlagfogyasztas_kmre()
+        {
+            return Uzemanyag_felhasznalas()/MegtettKilometerekSzama;
+        }
+
+
+        public void AdatKiir()
+        {
+            Console.WriteLine("Autók adatai");
+            Console.WriteLine($"Márka: {Marka}");
+            Console.WriteLine($"Tipus: {Tipus}");
+            Console.WriteLine($"Gyártási év: {GyartasiEv}");
+            Console.WriteLine($"Életkor: {Eletkor()} év");
+            Console.WriteLine($"Fogyasztás: {UzemanyagFogyasztas}l/100km");
+            Console.WriteLine($"Megtett kilométer {MegtettKilometerekSzama}km");
+            Console.WriteLine($"Összes felhasznált üzemanyag: {Uzemanyag_felhasznalas()}l");
+            Console.WriteLine($"Átlagos fogyasztás 1km-re: {Atlagfogyasztas_kmre()}liter/km");
+        }
+
+
+
+
+    }
+}
---
+++ autodoga/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autodoga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var toyota = ("Toyota","Corolla",2015,6.5,152000);
+
+            toyota.AdatKiir();
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
